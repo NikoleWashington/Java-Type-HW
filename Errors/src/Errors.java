6047c009c4db92cb46cb981b728d227ed1c41748
nwashington <nikolee1541@gmail.com>
2022-07-19 13:10:27 -0400

    added

---
+++ Errors/src/Errors.java
@@ -0,0 +1,19 @@
+
+public class Errors {
+
+	public static void main(String[] args) {
+		
+		int num1 = 4;
+		String dog = "Murphy";
+		int Num 2 = 3;
+		
+		System.out.println(dog + " the dog, has " + num1 + " legs!" );
+		
+		System.out.println(Num 2);
+		
+		
+		
+
+	}
+
+}
